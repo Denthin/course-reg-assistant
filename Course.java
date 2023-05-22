3c94a3800aef1713bbbb3ee6608fc15a8e30bcec
“chxhu” <cxhu@unc.edu>
2023-05-22 17:20:00 -0400

    Added priority

--- Course.java
+++ Course.java
@@ -3,11 +3,13 @@ public class Course {
     public final String purpose;
     public final int time;
     public final int credits;
+    public final int priority;
 
-    public Course(String abbr, String section, String purpose, int time, int credits) {
+    public Course(String abbr, String section, String purpose, int time, int credits, int priority) {
         this.name = String.join("-", abbr, section);
         this.purpose = purpose;
         this.time = time;
         this.credits = credits;
+        this.priority = priority;
     }
 }
